a424eca034c94dc48ac57879e60e8fbdd09ff009
BeaTowanda <bgonzalez.lia@gmail.com>
2022-02-15 16:35:25 -0300

    primer comit de nuevo

---
+++ controllers/mainController.js
@@ -0,0 +1,48 @@
+const { urlencoded } = require("express");
+
+/*const mainController = {
+    index: (req, res) => {
+      res.render("index", { about: about, menu: listaPlatos });
+    },
+    detalle: (req, res) => {
+      let plato = listaPlatos.find(plato => plato.id == req.params.menuId);
+      console.log(plato)
+      res.render("detalleMenu", { plato: plato });
+    },
+  };*/
+
+const productos= [ {
+                  codigo:"01-remera-redondo",
+                  nombre:"remera superman",
+                  descripcion:"Remera algodón 100 hilos , tipo Peruano, no térmico, manga Corta",
+                  /*imagen:url(public/img/remeraSupermanSinFondo.png),*/
+                  licencia:"superman",
+                  tipo:"Remera manga corta",
+                  color:"#00555",
+                  talle:"small",
+                  precio:8500,
+                  cantidad:200,
+                  descuento:10, 
+},] ;
+module.exports=productos;
+
+  const mainController = {
+    index: (req, res) => {
+        res.render("index"); 
+    },
+    login: (req, res) => {
+    res.render("login");
+    },
+    register: (req, res) => {
+        res.render("register");
+    },
+    altaProductos: (req, res) => {
+        res.render("altaProductos");
+    },
+    modificaProductos: (req, res) => {
+      res.render("modificaProductos");
+  },
+  };
+  
+  module.exports=mainController;
+
---
+++ routes/index.js
@@ -0,0 +1,24 @@
+/*var express = require('express');
+var router = express.Router();*/
+
+/* GET home page. */
+/*router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;*/
+/* para arriba es lo standard ***/
+
+/*aquí viene para GEEK */
+var express = require('express');
+var router = express.Router();
+const mainController = require('../controllers/mainController');
+ 
+/* GET home page. */
+router.get('/', mainController.index);
+router.get('/login', mainController.login);
+router.get('/register', mainController.register);
+router.get('/altaPRoductos', mainController.altaProductos);
+router.get('/modificaProductos', mainController.modificaProductos);
+
+module.exports = router;
